c11c8c1cc716d0c726d0973943491837843fbf35
suspicious-cheese <klaasvanloon@icloud.com>
2025-09-01 14:06:24 +0200

    home assistant config from nixos.wiki

--- host/home-server/configuration.nix
+++ host/home-server/configuration.nix
@@ -100,9 +100,28 @@
     ];
   };
 
+
+  {
+    services.home-assistant = {
+      enable = true;
+      extraComponents = [
+        # Components required to complete the onboarding
+        "esphome"
+        "met"
+        "radio_browser"
+      ];
+      config = {
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = {};
+      };
+    };
+  }
+  
+
   # Install firefox.
   programs.firefox.enable = true;
-
+  
   # Enable plex
   services.plex.enable = true;
   services.plex.openFirewall = true;
